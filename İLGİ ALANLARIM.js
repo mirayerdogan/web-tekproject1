230b2e464f8102fd1aa07933f94033d00882762e
Miray Erdoğan <132089798+mirayerdogan@users.noreply.github.com>
2023-05-19 23:55:53 +0300

    web-tekproject1
    web ödev

--- İLGİ ALANLARIM.js
+++ İLGİ ALANLARIM.js
@@ -1,62 +1,126 @@
-var movies = [
-  { title: "The Parazit", director: "Bong Joon-ho", year: 2019 },
-  { title: "Dangal", director: "Nitesh Tiwari", year: 2016 },
-  { title: "The Martian", director: "Ridley Scott", year: 2015 },
-  { title: "Bird Box", director: "Susanne Bier", year: 2018 },
-  { title: "Escape Room", director: "Adam Robitel", year: 2019 },
-  { title: "Friends", director: " David Crane , Marta Kauffman", year: 1994},
-  { title: "Suits", director: "Aaron Korsh", year: 2011}
+const filmListesi = document.getElementById('filmListesi');
+const müzikListesi = document.getElementById('müzikListesi');
+const sporListesi = document.getElementById('sporListesi');
+const doğaListesi = document.getElementById('doğaListesi');
+
+const filmler = [
+  'The Parazit',
+  'The Dangal',
+  'The The Martian',
+  'The Bird Box',
+  'The Escape Room',
+  'The Friends',
+  'The Suits',
+  'The Tom & Jerry',
+  'The Şirinler',
+  'The Stranger Things',
+  'Biz Kimden Kaçıyoruk Anne',
+  'Wednesday',
+  'Son Durak',
+  'Nefesini Tut',
+  'Recep İvedik',
+  'Hababam Sınıfı',
+  'Awake',
+  'Açlık'
 ];
 
-var sports = [
-  { name: "Tenis"},
-  { name: "Basketbol"},
-  { name: "Plates"},
-  { name: "Koşu"},
-  { name: "Yüzme"},
-  { name: "Voleybol"},
-  { name: "Kayak"}
-
+const müzikler = [
+  'Derinlerde',
+  'Bad Boy',
+  'Anlayamazsın',
+  'Calm Down',
+  'Middle of the Night',
+  'Tac Mahal',
+  'Shape of you',
+  'Astronaut in the Ocean',
+  'Yüreğimden Tut',
+  'Beyaz ve Sen',
+  'Fairytale',
+  'In my Eyes',
+  'Let Me Down ',
+  'Bilmezler Nasıl Sevdik',
+  'Sana Çıkıyor Yollar',
+  'Alışamadım',
+  'Sabır Ver',
+  'Love Story'
 ];
 
-var music = [
-  { title: "Derinlerde", artist: "Cem Adrian"},
-  { title: "Bad Boy", artist: "Marwa Loud"},
-  { title: "Fırtınadayım", artist: "Mabel Matiz"},
-  { title: "Anlayamazsın", artist: "Seksendört"},
-  { title: "Calm Down", artist: "Rema, Selena Gomez"},
-  { title: "Middle of the Night", artist: "Elley Duhé"},
-  { title: "Tac Mahal", artist: "Buray"},
-  { title: "Shape of You", artist: "Ed Sheeran "},
-  { title: "Astronaut in the Ocean", artist: "Masked Wolf"}
+const sporlar = [
+  'Tenis',
+  'Plates',
+  'Basketbol',
+  'Koşu',
+  'Yüzme',
+  'Voleybol',
+  'Kayak'
 ];
 
-function showMovies() {
-  var movieListHTML = "";
-  for (var i = 0; i < movies.length; i++) {
-    movieListHTML += '<div class="movie-item"><h3>' + movies[i].title + '</h3><p>Yönetmen: ' + movies[i].director + '</p><p>Yıl: ' + movies[i].year + '</p></div>';
-  }
-  document.getElementById("Movie-list").innerHTML = movieListHTML;
-}
+const doğa = [
+  'Gökyüzü',
+  'Orman',
+  'Ağaçlar',
+  'Güneş',
+  'Yağmur',
+  'Bitkiler',
+  'Hayvanlar',
+  'Deniz',
+];
 
-function showSports() {
-  var sportListHTML = "";
-  for (var i = 0; i < sports.length; i++) {
-    sportListHTML += '<div class="sport-item"><h3>' + sports[i].name + '</h3><p>' + '</p></div>';
-  }
-  document.getElementById("Sports-list").innerHTML = sportListHTML;
-}
 
-function showMusic() {
-  var musicListHTML = "";
-  for (var i = 0; i < music.length; i++) {
-    musicListHTML += '<div class="music-item"><h3>' + music[i].title + '</h3><p>Sanatçı: ' + music[i].artist + '</p></div>';
-  }
-  document.getElementById("Music-list").innerHTML = musicListHTML;
-}
 
-window.onload = function() {
-  showMovies();
-  showSports();
-  showMusic();
-};
+fetch('https://api.themoviedb.org/3/movie/550?api_key=273d5a63f37d57017dad56e3a2d0ef31', {
+  headers: {
+    'Authorization': '273d5a63f37d57017dad56e3a2d0ef31'
+  }
+})
+.then(response => response.json())
+.then(data => {
+  const filmListesi = document.getElementById('filmListesi');
+
+  filmler.forEach(film => {
+    const li = document.createElement('li');
+    li.textContent = film;
+    filmListesi.appendChild(li);
+  });
+})
+.catch(error => console.log(error));
+
+
+
+fetch('https://musicapi13.p.rapidapi.com/public/search/introspection', {
+  headers: {
+    'Authorization': 'ac705fa1f2msh768826e19727a31p106927jsnb7ae3df14f62'
+  }
+})
+.then(response => response.json())
+.then(data => {
+  const müzikListesi = document.getElementById('müzikListesi');
+
+  müzikler.forEach(müzik => {
+    const li = document.createElement('li');
+    li.textContent = müzik;
+    müzikListesi.appendChild(li);
+  });
+})
+.catch(error => console.log(error));
+
+
+
+fetch('https://api-football-v1.p.rapidapi.com/v3/timezone', {
+  headers: {
+    'Authorization': 'ac705fa1f2msh768826e19727a31p106927jsnb7ae3df14f62'
+  }
+})
+.then(response => response.json())
+.then(data => {
+  const sporListesi = document.getElementById('sporListesi');
+
+  sporlar.forEach(spor => {
+    const li = document.createElement('li');
+    li.textContent = spor;
+    sporListesi.appendChild(li);
+  });
+})
+.catch(error => console.log(error));
+
+
--- ŞEHRİM.js
+++ ŞEHRİM.js
@@ -1,69 +1,26 @@
-const slider = document.querySelector('.slider');
-const slides = document.querySelectorAll('.slide-container');
+var slayt = document.getElementsByClassName("slider")[0].getElementsByTagName("img");
+  var slaytSayisi = slayt.length;
+  var slaytNo = 0;
+  var i = 0;
 
-   let currentIndex = 0;
+  slaytGoster(slaytNo);
 
-   function showSlide(index) {
-       slides.forEach((slide, i) => {
-           if (i === index) {
-               slide.style.display = 'block';
-           } else {
-               slide.style.display = 'none';
-           }
-       });
-   }
+  function sonrakiSlayt() {
+    slaytNo++;
+    if (slaytNo >= slaytSayisi) slaytNo = 0;
+    slaytGoster(slaytNo);
+  }
 
-   function nextSlide() {
-       currentIndex++;
-       if (currentIndex >= slides.length) {
-           currentIndex = 0;
-       }
-       showSlide(currentIndex);
-   }
+  function oncekiSlayt() {
+    slaytNo--;
+    if (slaytNo < 0) slaytNo = slaytSayisi - 1;
+    slaytGoster(slaytNo);
+  }
 
-   function previousSlide() {
-       currentIndex--;
-       if (currentIndex < 0) {
-           currentIndex = slides.length - 1;
-       }
-       showSlide(currentIndex);
-   }
+  function slaytGoster(slaytNumarasi) {
+    for (i = 0; i < slaytSayisi; i++) {
+      slayt[i].style.display = "none";
+    }
+    slayt[slaytNumarasi].style.display = "block";
+  }
 
-   showSlide(currentIndex);
-
-   const prevButton = document.getElementById('prev-btn');
-   const nextButton = document.getElementById('next-btn');
-
-   prevButton.addEventListener('click', previousSlide);
-   nextButton.addEventListener('click', nextSlide);
-
-   var swiper = new Swiper('.slider-container', {
-       slidesPerView: 1,
-       spaceBetween: 10,
-       navigation: {
-         nextEl: '.swiper-button-next',
-         prevEl: '.swiper-button-prev',
-       },
-     });
-
-     var prevBtn = document.getElementById('prev-btn');
-var nextBtn = document.getElementById('next-btn');
-
-prevBtn.addEventListener('click', function() {
- swiper.slidePrev();
-});
-
-nextBtn.addEventListener('click', function() {
- swiper.slideNext();
-});
-
-
-var sliderContainer = document.querySelector('.slider-container');
-
-sliderContainer.addEventListener('mouseenter', function() {
- swiper.autoplay.stop();
-});
-
-sliderContainer.addEventListener('mouseleave', function() {
- swiper.autoplay.start();
-});
